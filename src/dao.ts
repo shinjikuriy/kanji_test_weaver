30ace2efa8e6e19e1f0be54c17ce64bc20218976
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2025-01-09 17:45:55 +0900

    feat: add DAO functions to select kanji by chapter and retrieve chapters by textbook ID

--- src/dao.ts
+++ src/dao.ts
@@ -10,6 +10,16 @@ export const selectKanjiByCharacter = db.prepare(`
   where kanji.character = $character;
   `)
 
+export const selectKanjiByChapter = db.prepare(`
+  select
+    kanji.id, kanji.character, textbooks.title, kanji.chapter
+    from kanji
+    join textbooks on kanji.textbook_id = textbooks.id
+  where kanji.textbook_id = $textbook_id
+    and kanji.chapter = $chapter
+  order by kanji.id;
+  `)
+
 export const selectWordsByKanjiId = db.prepare(`
   select words.id, words.word, textbooks.title, words.chapter
     from words
@@ -24,4 +34,12 @@ export const selectSentencesByKanjiId = db.prepare(`
     JOIN kanji_sentences ks on sentences.id = ks.sentence_id
     JOIN textbooks on sentences.textbook_id = textbooks.id
   where ks.kanji_id = $kanjiId;
+  `)
+
+export const selectTextbooks = db.prepare(`
+  select * from textbooks order by id;
+  `)
+
+export const selectChaptersByTextbookId = db.prepare(`
+  select distinct kanji.chapter from kanji where textbook_id = $textbookId order by kanji.chapter;
   `)
