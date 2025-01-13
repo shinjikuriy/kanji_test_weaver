82d16eda8e836d3c0b89ac030cb00ab6607bac66
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2025-01-13 11:00:40 +0900

    feat: add chapters fetching functionality for textbooks

--- src/client.tsx
+++ src/client.tsx
@@ -13,16 +13,47 @@ function App() {
     setTextbooks(textbooks)
   }
 
+  const [chapters, setChapters] = useState<number[]>([])
+  const fetchChapters = async (textbookId: number) => {
+    const response = await fetch(`/api/textbooks/${textbookId}/chapters`)
+    if (!response.ok) {
+      throw new Error('Failed to fetch chapters')
+    }
+    const chapters = await response.json()
+    setChapters(chapters.map((chapter: { chapter: number }) => chapter.chapter))
+  }
+
   return (
     <>
       <div>
         <h1>Hello, World!</h1>
+        <button onClick={fetchTextbooks}>show textbooks</button>
       </div>
+      {textbooks.length > 0 && (
       <div>
         <h2>Textbooks</h2>
-        <button onClick={fetchTextbooks}>show textbooks</button>
-        <p>{textbooks.map(textbook => <button>textbook.title</button>)}</p>
+        <div>
+          {textbooks.map((textbook) => (
+            <button
+              key={textbook.id}
+              onClick={() => fetchChapters(textbook.id)}
+            >
+              {textbook.title}
+            </button>
+          ))}
+        </div>
       </div>
+      )}
+      {chapters.length > 0 && (
+        <div>
+          <h2>Chapters</h2>
+          <div>
+            {chapters.map((chapter) => (
+              <button key={chapter}>{chapter}</button>
+            ))}
+          </div>
+        </div>
+      )}
     </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,24 @@ import * as dao from './dao'
 
 const app = new Hono()
 
-app.use('/*', serveStatic({
-  root: './',
-  rewriteRequestPath: (path) => path.replace(/\/static/, 'dist/static'),
-}))
+app.use(
+  '/*',
+  serveStatic({
+    root: './',
+    rewriteRequestPath: (path) => path.replace(/\/static/, 'dist/static'),
+  })
+)
 
 app.get('/', (c) => {
   return c.html(
     <html lang="ja">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Kanji Test Weaver</title>
-      <script type="module" src="/static/client.js"></script>
-    </head>
-    <body></body>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Kanji Test Weaver</title>
+        <script type="module" src="/static/client.js"></script>
+      </head>
+      <body></body>
     </html>
   )
 })
@@ -26,6 +29,19 @@ app.get('/', (c) => {
 app.get('/api/textbooks', (c) => {
   const textbooks = dao.selectTextbooks.all()
   return c.json(textbooks)
+})
+
+app.get('/api/textbooks/:textbookId/chapters', (c) => {
+  const { textbookId } = c.req.param()
+
+  // check if textbookId is an integer
+  if (!Number.isInteger(Number(textbookId))) {
+    c.status(400)
+    return c.json({ error: 'textbookId must be an integer' })
+  }
+
+  const chapters = dao.selectChaptersByTextbookId.all(Number(textbookId))
+  return c.json(chapters)
 })
 
 export default app
